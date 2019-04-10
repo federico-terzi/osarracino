c9100f4b6591acba6915b73a55c1fa6b1889a13e
max <massimo.schembri18@gmail.com>
2019-04-10 20:01:21 +0200

    Add cli parameters. Add white|black options for cli invocation.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,29 @@
 #include <unistd.h>
 #include "Board.h"
 #include "Connector.h"
+#include <map>
+#include <locale>
 
-int main() {
+const int PLAYER_COLOR = 1;
+
+std::map<std::string, int> ports {{"white", 5800},{"black",5801}};
+
+int main(int argc, char **argv) {
+    std::string player_color;
+    if (argc < 2) {
+        std::cout << "Missing Player Color {Black|White}" << std::endl;
+        std::cout << "Starting as white" << std::endl;
+        player_color= "white";
+    } else {
+        player_color = std::string(argv[PLAYER_COLOR]);
+    }
+
     std::cout << "Hello, World!" << std::endl;
     Board b;
     b.load_board("{\"board\":[[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\",\"BLACK\",\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\"],[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\"],[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"WHITE\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\"],[\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"WHITE\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\"],[\"BLACK\",\"BLACK\",\"WHITE\",\"WHITE\",\"KING\",\"WHITE\",\"WHITE\",\"BLACK\",\"BLACK\"],[\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"WHITE\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\"],[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"WHITE\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\"],[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\",\"EMPTY\"],[\"EMPTY\",\"EMPTY\",\"EMPTY\",\"BLACK\",\"BLACK\",\"BLACK\",\"EMPTY\",\"EMPTY\",\"EMPTY\"]],\"turn\":\"WHITE\"}");
     std::cout << b;
 
-    Connector connector{5800};
+    Connector connector{ports[player_color]};
     connector.send_name("Pippo");
 
     b.load_board(connector.receive_string());
