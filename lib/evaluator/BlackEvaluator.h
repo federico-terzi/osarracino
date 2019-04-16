8be9be32f4b5c595167543e2134031b7b02e3057
max <massimo.schembri18@gmail.com>
2019-04-16 11:04:04 +0200

    Add black win conditions.

--- lib/evaluator/BlackEvaluator.h
+++ lib/evaluator/BlackEvaluator.h
@@ -9,11 +9,45 @@
 #include <vector>
 #include <tuple>
 #include <array>
+#include <unordered_map>
+#include <functional>
+
+enum Direction {Up=1, Down=2, Right=3, Left=4, None=false};
+
+const int BLANK_WG = 0;
+const int CYAN_WG = 1;
+const int PURPLE_WG = 2;
+const int ALL_COLS_COVERED = 10;
+const int ALL_ROWS_COVERED = 10;
+const int KINGS_OPP = 1;
+const int EZPZ = 5000;
 
 class BlackEvaluator : public Evaluator<BlackEvaluator>{
 public:
-    int evaluate(const Board &b) const;
+    BlackEvaluator();
+    std::unordered_map<Position, Direction> near_throne;
+    std::unordered_map<Direction, std::function<bool(Direction,const Board&)>> near_checks;
+
+    int evaluate(const Board &b) ;
+    bool simple_win_condition(const Board &b);
+    bool throne_win_condition(const Board &b);
+    bool near_throne_win_condition(const Board &b);
+    bool is_king_in_throne(const Board &b);
+    Direction is_king_near_throne(const Board &b);
+
     static std::pair<std::vector<Position>, std::array<std::bitset<9>, 9>> Get_empty_and_to_move(const Board &b);
+    int color_matrix[9][9] = {
+            {BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG},
+            {BLANK_WG, BLANK_WG, PURPLE_WG, BLANK_WG, CYAN_WG, BLANK_WG, PURPLE_WG},
+            {BLANK_WG, PURPLE_WG, BLANK_WG, CYAN_WG, BLANK_WG, CYAN_WG, BLANK_WG, PURPLE_WG, BLANK_WG },
+            {BLANK_WG, BLANK_WG, CYAN_WG, BLANK_WG, BLANK_WG, BLANK_WG, CYAN_WG, BLANK_WG, BLANK_WG},
+            {BLANK_WG, CYAN_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, CYAN_WG, BLANK_WG},
+            {BLANK_WG, BLANK_WG, CYAN_WG, BLANK_WG, BLANK_WG, BLANK_WG, CYAN_WG, BLANK_WG, BLANK_WG},
+            {BLANK_WG, PURPLE_WG, BLANK_WG, CYAN_WG, BLANK_WG, CYAN_WG, BLANK_WG, PURPLE_WG, BLANK_WG},
+            {BLANK_WG, BLANK_WG, PURPLE_WG, BLANK_WG, CYAN_WG, BLANK_WG, PURPLE_WG},
+            {BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG, BLANK_WG},
+
+    };
 };
 
 
