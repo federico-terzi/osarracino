153e2ae5d7df2af81ac691c1eade8e4a81fa35b3
Federico Terzi <federicoterzi96@gmail.com>
2019-04-24 10:08:15 +0200

    First part of the new main refactor

---
+++ cli/profiles/PlayerProfile.h
@@ -0,0 +1,12 @@
+//
+// Created by freddy on 23/04/19.
+//
+
+#ifndef OSARRACINO_PLAYERPROFILE_H
+#define OSARRACINO_PLAYERPROFILE_H
+
+class PlayerProfile {
+
+};
+
+#endif //OSARRACINO_PLAYERPROFILE_H
